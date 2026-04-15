bcf79beb4dbfdb831c0bf515ba2a88304447ee15
felipesenajw <146647935+felipesenajw@users.noreply.github.com>
2026-04-14 21:29:35 -0300

    Add files via upload
    Versão inicial do app

---
+++ sw.js
@@ -0,0 +1,12 @@
+const cacheName = 'calc-taxa-v1';
+const assets = ['./', './index.html'];
+
+// Instalação: Guarda os ficheiros no cache
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+// Responde mesmo sem internet
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
