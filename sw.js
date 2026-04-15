67e6d7347fb152d9c02c527e373ac082dea158a4
felipesenajw <146647935+felipesenajw@users.noreply.github.com>
2026-04-15 07:07:57 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,20 @@
-const cacheName = 'calc-taxa-v2';
-const assets = ['./', './index.html'];
+const cacheName = 'calc-taxa-v3'; // Mudámos para v3
+const assets = ['./', './index.html', './manifest.json'];
 
-// Instalação: Guarda os ficheiros no cache
 self.addEventListener('install', e => {
+  self.skipWaiting(); // Força o novo Service Worker a ativar-se imediatamente
   e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
 
-// Responde mesmo sem internet
+self.addEventListener('activate', e => {
+  // Apaga qualquer cache que não seja o v3
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.filter(key => key !== cacheName).map(key => caches.delete(key)));
+    })
+  );
+});
+
 self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
